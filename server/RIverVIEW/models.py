afbe1a722e1a6014f6cc0d4271589ee580f58605
rnjsgo <102651155+rnjsgo@users.noreply.github.com>
2022-11-20 18:30:54 +0900

    main화면

--- server/RIverVIEW/models.py
+++ server/RIverVIEW/models.py
@@ -1,36 +1,32 @@
 from django.db import models
+
 # Create your models here.
+
 class ProductModel(models.Model):
     class Meta:
         db_table = "product"
+    def __str__(self):
+        return self.product_name
     product_url = models.CharField(max_length=256, default='')
     product_name = models.CharField(max_length=256, default='')
-    product_num = models.IntegerField(null=True)
+    product_id = models.IntegerField(null=True)
     categories = models.TextField(null=True)
     img_src = models.CharField(max_length=256, default='')
     price = models.CharField(max_length=32, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    total_value = models.FloatField(null=True, default=0)
-    pos_neg_rate = models.IntegerField(null=True, default=0)
-    search_value = models.IntegerField(null=True)
-
 
 class ReviewModel(models.Model):
     class Meta:
         db_table = "review"
     product_id = models.ForeignKey(ProductModel, on_delete=models.CASCADE,default='')
-    review = models.CharField(max_length=256)
-    score = models.FloatField(null=True, default=0)
-    keywords = models.CharField(max_length=256)
-    morph = models.CharField(max_length=256, default='')
-    xai_vale = models.CharField(max_length=256, default='')
-    date = models.CharField(max_length=256, default='')
+    review = models.CharField(max_length=1000)
 
 class ProductKeyword(models.Model):
     class Meta:
         db_table = "PDkeyword"
     product_id = models.ForeignKey(ProductModel, on_delete=models.CASCADE, default='')
     keyword = models.CharField(max_length=20, default='')
-    summarization = models.CharField(max_length=256, default='')
-    keyword_positive = models.FloatField(default=60)
+    summarization = models.CharField(max_length=256, default='') #해당 키워드의 대표 리뷰
+    keyword_score = models.FloatField(default=0) #키워드의 점수 (긍정이면 + 부정이면 -)
+    keyword_frequency = models.IntegerField(null=True)
--- server/analysisApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'analysisApp'
-urlpatterns = [
-    path('',views.index, name='index'),
-    path('/details/<int:product_id>/', views.view_details, name= 'details')
-]
