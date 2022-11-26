028e4a73a58fb4739766a94cbad486238538046e
rnjsgo <102651155+rnjsgo@users.noreply.github.com>
2022-11-26 22:06:07 +0900

    1

--- server/RIverVIEW/views.py
+++ server/RIverVIEW/views.py
@@ -8,8 +8,6 @@ from collections import Counter, defaultdict
 
 def foward_home(request):
     if request.method == 'GET':
-        T_Products = [ProductModel.objects.all().order_by('-search_value')[i * 5:i * 5 + 5] for i in range(4)]
-        R_Products = [ProductModel.objects.all().order_by('-created_at')[i * 5:i * 5 + 5] for i in range(4)]
         return render(request, 'RIverVIEW/main.html')
 
 def view_details(request, product_id):
